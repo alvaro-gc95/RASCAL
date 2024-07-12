cad540c7984dc2b45d60d049168e985164f32781
Álvaro González-Cervera <109213876+alvaro-gc95@users.noreply.github.com>
2024-07-12 15:40:02 +0200

    Update analysis.py

--- rascal/analysis.py
+++ rascal/analysis.py
@@ -810,7 +810,7 @@ def clean_df(df):
     """
     assert isinstance(df, pd.DataFrame), "df needs to be a pd.DataFrame"
     df.dropna(inplace=True)
-    indices_to_keep = ~df.isin([np.nan, np.inf, -np.inf]).any(1)
+    indices_to_keep = ~df.isin([np.nan, np.inf, -np.inf]).any(axis=1)
     return df[indices_to_keep].astype(np.float64)
 
 
